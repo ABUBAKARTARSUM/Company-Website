82dc92186af1a88073c151d5a6536b272c631306
ABUBAKARTARSUM <80694589+ABUBAKARTARSUM@users.noreply.github.com>
2022-06-16 19:04:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// AOS Script start
+AOS.init({
+  duration: 900,
+});
+// AOS Script end
+// GSAP Script start
+gsap.registerPlugin(TextPlugin);
+gsap.to('.descTestimonial', { duration: 2.9, delay: 3, text: '" Bekerja dengan suasana hati yang lebih asik, menyenangkan dan mempelajari hal baru setiap harinya. "' });
+// GSAP Script end
